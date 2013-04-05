e39b9334ca535b64fb016a11fa68a75ac0d6ce27
Devon Latzen <dclatzen@gmail.com>
2013-04-05 04:27:30 -0700

    Adding bandcamp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandcamp::Application.config.secret_token = '53c30208b7da7ed3bc57c106eb4ae5970bbac473aab7b5278abc03d5a235c2069a698656fbe9d6a4ebc0590dba46cec811cf36480b6e39d86416a86fe88c5dfb'
